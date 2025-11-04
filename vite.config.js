5e98fd14329745b2d16b5dbfcda4b71e4ce8bc07
yamileth-quispe <jhoquispeespinoza@crackthecode.la>
2025-11-04 12:28:04 -0500

    Agrego imágenes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Digital-Dreamers/', // 👈 ESTA LÍNEA ES CLAVE PARA GITHUB PAGES
 })
